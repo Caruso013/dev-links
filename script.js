fa2b505b4ff5534adcc45d97be91043acc35acb9
Caruso013 <pedromk84@gmail.com>
2023-02-01 03:24:20 -0300

    fix a bug

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function toggleMode() {
+function toogleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
+  //pegar a tag img
   const img = document.querySelector("#profile img")
 
+  //substituir a image
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/Avatar-Light.png")
+    img.setAttribute("src", "./assets/avatar-Light.png")
   } else {
     img.setAttribute("src", "./assets/Avatar-Dark.png")
   }
